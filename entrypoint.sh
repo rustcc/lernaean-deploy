26e1303b347857c1b311f886eee81f1e56cf9bba
DCjanus <dcjanus@dcjanus.com>
2019-06-10 10:12:17 +0800

    add DNS conf

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ rm -f /etc/localtime
 ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 echo "StrictHostKeyChecking no" >> /root/.ssh/config
 
+echo "nameserver 119.29.29.29" > /etc/resolv.conf
+echo "nameserver 114.114.114.114" >> /etc/resolv.conf
+
 lernaean \
     --dl https://crates.longhash.com.cn/api/v1/crates \
     --origin git@code.aliyun.com:rustcc/crates.io-index.git
